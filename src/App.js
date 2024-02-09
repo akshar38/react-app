8ad0eb1124735e11d58615fb1854d862872cf569
akshar38 <aksharlathiya9999@gmail.com>
2024-02-09 14:55:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank" 
           rel="noopener noreferrer"
         >
-        Welcome To react
+        Welcome To react app
         </a>
       </header>
     </div>
