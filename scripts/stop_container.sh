7e7e209e75a677be5deed027ab70d79b4b3a5f65
akshar38 <aksharlathiya9999@gmail.com>
2024-02-09 16:07:43 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,11 +2,11 @@
 set -e
 
 # Get the container ID running on port 3000
-containerid=$(/usr/bin/docker ps -q)
+containerid=$(sudo docker ps -q)
 
 # Remove the container
 if [ -n "$containerid" ]; then
-    /usr/bin/docker rm -f "$containerid"
+    sudo docker rm -f "$containerid"
 else
     echo "No container running"
 fi
