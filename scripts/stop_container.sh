b3f6e9d4c09caca5c1ee782beb48c3af4410441a
akshar38 <aksharlathiya9999@gmail.com>
2024-02-11 19:36:54 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,7 +2,7 @@
 set -e
 sudo su
 # Get the container ID running on port 3000
-containerid=$(sudo docker ps -q)
+containerid=$(docker ps -q)
 
-sudo docker rm -f "$containerid"
+docker rm -f "$containerid"
 
