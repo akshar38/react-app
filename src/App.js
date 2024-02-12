e78152ce913428edece6fb87e35b0bb16d02acfc
akshar38 <aksharlathiya9999@gmail.com>
2024-02-12 09:16:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank" 
           rel="noopener noreferrer"
         >
-        Welcome To react app
+        Welcome To react ci/cd pipeline app
         </a>
       </header>
     </div>
