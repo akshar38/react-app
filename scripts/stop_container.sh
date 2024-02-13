c518bf2700dc56da0925586440b53358b9911c9d
akshar38 <aksharlathiya9999@gmail.com>
2024-02-13 13:30:21 +0530

    first commit

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -3,8 +3,10 @@ set -e
 
 # Get the container ID running on port 3000
 containerid=$(sudo docker ps -q)
+imageid=$(sudo docker images -q)
 
 if [ -n "$containerid" ]; then
+    sudo docker rmi -f "$imageid"
     sudo docker rm -f "$containerid"
 else
     echo "No container running"
