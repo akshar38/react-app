cc39dde2f11659dad7ddb6f0943b93c47a48e540
akshar38 <aksharlathiya9999@gmail.com>
2024-02-22 10:19:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,7 +20,6 @@ function App() {
         </a>
       </header>
     </div>
-
   );
 }
 
